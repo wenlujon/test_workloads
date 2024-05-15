6cbda6fdb4546b9b1851e79a21df778cb746e03e
wenlujon <wenlujon@163.com>
2024-05-15 17:11:25 +0800

    fix mysql port

--- tests/run_sysbench.sh
+++ tests/run_sysbench.sh
@@ -3,6 +3,7 @@
 #affinity="taskset -c 0-15"
 affinity=""
 SERVERIP=$1
+PORT=3003
 #sync && echo 3 > /proc/sys/vm/drop_caches
 #mode=oltp_read_only
 mode=oltp_write_only
@@ -17,15 +18,15 @@ for nthread in 256
 do
         echo "${time} $mode Testing Ali config on mysql-8.0.25: nthread ${nthread}"
 
-        $affinity sysbench $mode --db-ps-mode=auto --mysql-host=$SERVERIP --mysql-port=3001 --mysql-user=sysbench --mysql-password=password --mysql-db=sysdb --tables=100 --table_size=400000 --time=300 --report-interval=1 --threads=64 cleanup
+        $affinity sysbench $mode --db-ps-mode=auto --mysql-host=$SERVERIP --mysql-port=$PORT --mysql-user=sysbench --mysql-password=password --mysql-db=sysdb --tables=100 --table_size=400000 --time=300 --report-interval=1 --threads=64 cleanup
         echo "===> finish cleanup"
         sleep 1
 
-        $affinity sysbench $mode --db-ps-mode=auto --mysql-host=$SERVERIP --mysql-port=3001 --mysql-user=sysbench --mysql-password=password --mysql-db=sysdb --tables=100 --table_size=400000 --time=300 --report-interval=1 --threads=64 prepare
+        $affinity sysbench $mode --db-ps-mode=auto --mysql-host=$SERVERIP --mysql-port=$PORT --mysql-user=sysbench --mysql-password=password --mysql-db=sysdb --tables=100 --table_size=400000 --time=300 --report-interval=1 --threads=64 prepare
         echo "===> finish prepare"
         sleep 1
 
-        $affinity sysbench $mode --db-ps-mode=auto --mysql-host=$SERVERIP --mysql-port=3001 --mysql-user=sysbench --mysql-password=password --mysql-db=sysdb --tables=100 --table_size=400000 --warmup-time=${warmup_time} --time=${run_time} --report-interval=10 --threads=${nthread} run
+        $affinity sysbench $mode --db-ps-mode=auto --mysql-host=$SERVERIP --mysql-port=$PORT --mysql-user=sysbench --mysql-password=password --mysql-db=sysdb --tables=100 --table_size=400000 --warmup-time=${warmup_time} --time=${run_time} --report-interval=10 --threads=${nthread} run
 
         echo "===> finish run"
         sleep 1
